7b91e08b9ce0417f3218fe0aa797e29e96bc8776
Andrew Witten <awitten1@terpmail.umd.edu>
2025-09-24 13:59:47 -0400

    another example

--- example/example.c
+++ example/example.c
@@ -10,3 +10,26 @@ void NoWAR(int param) {
   y = param;
   x = y;
 }
+
+int a, x, w, b, z;
+
+int IN();
+
+void WARFromPaper() {
+   if (a > 0) {
+      x = w;
+      w = 5;
+      b = 2;
+   } else {
+      int i = IN();
+      if (i>1) {
+        x = 6;
+        y = 7;
+        b = 9;
+      } else {
+        x = 7;
+        z = 8;
+        //w = 5;
+      }
+  }
+}
